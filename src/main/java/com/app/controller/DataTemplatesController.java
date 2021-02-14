db96340858d79ed1f360f22ab8af0ae7ddc28f69
Kiran Thota <kiranthota17@gmail.com>
2021-02-14 19:41:50 +0530

    Added APIs

--- src/main/java/com/app/controller/DataTemplatesController.java
+++ src/main/java/com/app/controller/DataTemplatesController.java
@@ -1,8 +1,17 @@
 package com.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.domain.DataTemplates;
@@ -11,20 +20,68 @@ import com.app.repository.DataTemplatesRepository;
 @RestController
 @RequestMapping("/app")
 public class DataTemplatesController {
-	
+
+	Logger log = LoggerFactory.getLogger(DataTemplatesController.class);
+
 	@Autowired
 	DataTemplatesRepository dataTemplatesRepository;
 
-	@GetMapping("/getDataTempaltes")
-	public DataTemplates getDataTempaltes() {
+	@GetMapping("/getDataTemplates")
+	public List<DataTemplates> getDataTempaltes(@RequestParam(required=false) Long id, @RequestParam Boolean getAll) 
+	{
 		try {
-			System.out.println("Get APi Called");
-			System.out.println("Size of Data Templates: ");//+ dataTemplatesRepository.findAll().size());
+			if( getAll == true )
+				return dataTemplatesRepository.findAll();
+			else {
+				List<DataTemplates> list = new ArrayList<DataTemplates>();
+				list.add(dataTemplatesRepository.findById(id).get());
+				return list;
+			}
 		}
 		catch(Exception exp) {
-			exp.printStackTrace();
+			log.error("Catch_Exception: In getDataTemplates, ",exp);
 		}
 		return null;
 	}
-	
+
+	@DeleteMapping("/deleteDataTemplates")
+	public List<DataTemplates> deleteDataTempalte(@RequestParam(required=false) Long id, @RequestParam Boolean deleteAll) 
+	{
+		try 
+		{
+			log.info("Delete Data Templates: {} or id: {}",deleteAll,id);
+			if( deleteAll == true )
+				dataTemplatesRepository.deleteAll();
+			else 
+				dataTemplatesRepository.deleteById(id);
+			log.info("Size of Data Templates After Deleting: "+dataTemplatesRepository.findById(id));
+			return dataTemplatesRepository.findAll();
+		}
+		catch(Exception exp) {
+			log.error("Catch_Exception: In getDataTemplates, ",exp);
+		}
+		return null;
+	}
+
+	@PostMapping("/saveOrUpdateDataTemplates")
+	public List<DataTemplates> saveOrUpdateDataTemplates(@RequestBody List<DataTemplates> dtList)
+	{
+		try {
+			dtList.forEach( v ->{
+				DataTemplates dt = new DataTemplates();
+				dt = v;
+				dt.setId(v.getId() != null ? v.getId() : null);
+				log.info("Data Template: "+dt.toString());
+				dataTemplatesRepository.save(v);
+			});
+			log.info("Saved Data Templates");
+			return dataTemplatesRepository.findAll();
+		}
+		catch(Exception exp)
+		{
+			log.error("Catch_Exception: In saveDataTemplates, ",exp);
+		}
+		return null;
+	}
+
 }
